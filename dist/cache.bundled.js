2ff095765e504ee362cbf8523a033eb948c35770
Jan Baykara <jan@baykara.co.uk>
2017-10-06 17:19:05 -0300

    Fix: options.cache.exclude.query was badly tested for default values

--- dist/cache.bundled.js
+++ dist/cache.bundled.js
@@ -2508,7 +2508,7 @@ function setupCache() {
   config.debug = config.debug || false;
 
   config.exclude = config.exclude || {};
-  config.exclude.query = config.exclude.query || true;
+  config.exclude.query = config.exclude.query !== undefined ? config.exclude.query : true;
   config.exclude.paths = config.exclude.paths || [];
   config.exclude.filter = config.exclude.filter || null;
 
